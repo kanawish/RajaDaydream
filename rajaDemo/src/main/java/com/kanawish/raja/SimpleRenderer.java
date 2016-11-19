4ffbe2f7854b60b84e8dfb7ab394f6cbfa8ad04d
Etienne Caron <etienne.caron@gmail.com>
2016-11-19 15:45:27 -0500

    Latest / Greatest

--- rajaDemo/src/main/java/com/kanawish/raja/SimpleRenderer.java
+++ rajaDemo/src/main/java/com/kanawish/raja/SimpleRenderer.java
@@ -15,9 +15,7 @@ import org.rajawali3d.materials.Material;
 import org.rajawali3d.materials.methods.DiffuseMethod;
 import org.rajawali3d.materials.textures.ATexture;
 import org.rajawali3d.math.vector.Vector3;
-import org.rajawali3d.postprocessing.PostProcessingManager;
 import org.rajawali3d.primitives.Cube;
-import org.rajawali3d.primitives.Plane;
 import org.rajawali3d.primitives.Sphere;
 
 class SimpleRenderer extends BaselineRenderer {
@@ -58,9 +56,14 @@ class SimpleRenderer extends BaselineRenderer {
             cube.setMaterial(material);
             getCurrentScene().addChild(cube);
 
+            Sphere sphere = new Sphere(0.5f,10,10);
+            sphere.setMaterial(material);
+            getCurrentScene().addChild(sphere);
+            animateOrbit(new Vector3(0),sphere);
+
             // Camera,
-            getCurrentCamera().setPosition(0, 2, 3);
-            getCurrentCamera().setLookAt(0, 0, 0);
+            getCurrentCamera().setPosition(0, 5, 4);
+            getCurrentCamera().setLookAt(0.0, 0.0, -0.1);
 
             // Action!
             RotateOnAxisAnimation anim = new RotateOnAxisAnimation(new Vector3(1,1,1), 360);
@@ -68,11 +71,22 @@ class SimpleRenderer extends BaselineRenderer {
             anim.setDurationMilliseconds(20000);
             anim.setRepeatMode(Animation.RepeatMode.INFINITE);
             getCurrentScene().registerAnimation(anim);
-            anim.play();
+//            anim.play();
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private void animateOrbit(Vector3 focalPoint, ATransformable3D transformable3D) {
+        EllipticalOrbitAnimation3D lightAnim = new EllipticalOrbitAnimation3D(
+                new Vector3(1,0,0), new Vector3(3, 0, 0), Vector3.getAxisVector(Vector3.Axis.Y), 0, 360,
+                EllipticalOrbitAnimation3D.OrbitDirection.CLOCKWISE);
+        lightAnim.setDurationMilliseconds(2000);
+        lightAnim.setRepeatMode(Animation.RepeatMode.INFINITE);
+        lightAnim.setTransformable3D(transformable3D);
+        getCurrentScene().registerAnimation(lightAnim);
+        lightAnim.play();
     }
 
     @Override
